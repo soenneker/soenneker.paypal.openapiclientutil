237396ffbc4a0f9a87129e86c7d4a1ff352ef518
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:32 -0500

    xml commenting

--- src/Soenneker.PayPal.OpenApiClientUtil/Abstract/IPayPalOpenApiClientUtil.cs
+++ src/Soenneker.PayPal.OpenApiClientUtil/Abstract/IPayPalOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.PayPal.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IPayPalOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<PayPalOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
